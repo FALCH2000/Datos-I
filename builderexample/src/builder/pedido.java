81f8cb249533a09e7c2d43dad0928509559712b7
fatimaleiva <fatimaleiva@192.168.100.12>
2019-08-28 23:45:15 -0600

    In this commit I create a new instance of Jefe class with diferents components predeterminates

---
+++ builderexample/src/builder/pedido.java
@@ -0,0 +1,22 @@
+package builder;
+
+public class pedido {
+
+	public static void main(String[] args) {
+		jefe crear= new jefe();
+		builder aguacate= new avocado();
+		builder de_la_casa= new casa();
+		
+		crear.set_emparedado(de_la_casa);
+		crear.hacer_emparedado();
+		System.out.print(crear.get_emparedado());
+		
+		jefe crear2= new jefe();
+		crear2.set_emparedado(aguacate);
+		crear2.hacer_emparedado();
+		System.out.print(crear2.get_emparedado());
+		
+
+	}
+
+}
